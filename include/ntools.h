aa6e4595df42eea9c8e72c1cd283b09d340543ed
Nikhil Karmakar <nikhilkarmakar4020@gmail.com>
2026-03-12 21:41:52 +0530

    add: from the ntype.h a few tools that can help a lots of thing in the file

---
+++ include/ntools.h
@@ -0,0 +1,39 @@
+#ifndef NTOOLS_H
+#define NTOOLS_H
+
+#define not !=
+#define is ==
+#define and &&
+#define or ||
+#define ifnot(cond) if(!(cond))
+
+enum buffer { 
+    base_buffer = 1024,
+    small_base_buffer = 128,
+    
+    buffer_4x = (base_buffer * 4), 
+    buffer_8x = (base_buffer * 8), 
+    buffer_16x = (base_buffer * 16), 
+    buffer_24x = (base_buffer * 24),
+
+    small_buffer_2x = (small_base_buffer * 2),
+    small_buffer_4x = (small_base_buffer * 4),
+    small_buffer_6x = (small_base_buffer * 6)
+};
+
+/*
+    Types 
+*/
+typedef char *cstring; // char-array as string
+
+typedef enum {
+    False = 0,
+    True  = 1
+} Bool;
+
+
+inline static const char* toBoolStr(Bool value){
+    return value ? "True" : "False";
+}
+
+#endif
