62690a8ec71423deecbb4c122f8af21af9130294
Nikhil Karmakar <nikhilkarmakar4020@gmail.com>
2026-03-14 01:06:28 +0530

    TEST: of nArrayList.*

--- examples/list.c
+++ examples/list.c
@@ -4,7 +4,10 @@
 int main(void){
     printf("\n=====================================================================\n\n");
 
-    ArrayList list = new_init();
+    ArrayList list = new_array_capacity(1,2);
+    arr_print(list);
+
+    for(int i = 1; i <= 5; i++) arr_add(&list, i);
     arr_print(list);
     arr_print_extra(list);
     
