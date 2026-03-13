79117631cd9b6478b43a52014e2ab995030cb081
Nikhil Karmakar <nikhilkarmakar4020@gmail.com>
2026-03-13 16:25:59 +0530

    add: basic TODO stuff that was on my list to add on the ArrayList. But still some ADV. is left

--- include/nArrayList.h
+++ include/nArrayList.h
@@ -5,21 +5,16 @@
 /*
 TODO: 
 
-1. calculate() -> will calculate all these max,min,avg & etc.
-2. calculate_clean() -> it can reset to zero everything whcih is not imp
-3.arr_merge() add 2 array list
-arr_sublist
-
+ADVANCE FEATURES
 
 1. function as agrs
 like => arr_for_each() and arr_filter()
 
-example of the function
+example of the function:
 arr_filter(ArrayList arr, Bool (*predicate)(int))
 
 
 Iterator can be cooked
-
 
 */
 
@@ -51,8 +46,9 @@ ArrayList new_range_to(int start, int end);
 ArrayList new_range_(int end);
 ArrayList new_range_reverce(int start, int end, int update);
 
+void arr_free(ArrayList *array_list); 
 
-void arr_free(ArrayList *array_list);
+/* ADD GET REMOVE */
 
 void arr_add(ArrayList arrary_list, int value);
 void arr_addAt(ArrayList arrary_list,int index ,int value);
@@ -72,15 +68,35 @@ int arr_remove_last(ArrayList *arrary_list);
 int arr_remove_val(ArrayList *arrary_list, int value); // return index 
 int arr_remove_all(ArrayList *arrary_list, int value); // return the len
 
-// searching
+/* BASIC ARRAY OPERATION */
+
 int arr_contain(ArrayList *arrary_list, int key); // return index 
 Bool arr_contain(ArrayList *arrary_list, int key);
 Bool arr_in(ArrayList *arrary_list1, ArrayList *arrary_list2);
 
 Bool arr_is_empty(ArrayList *arrary_list);
+void arr_reverce(ArrayList *arrary_list);
+ArrayList arr_sublist(ArrayList arrary_list, int start, int end);
+ArrayList arr_sublist_n(ArrayList arrary_list, int end);
+ArrayList arr_merge_return(ArrayList *list1 , ArrayList *list2);
+ArrayList arr_merge_size(ArrayList *list1 , ArrayList *list2, int list2_start, int list2_end);
+void arr_merge(ArrayList *to_list , ArrayList *from_list);
+
+/* 
+        == CALCULATE FUNCTION ==
+
+    % Manage the extra Stuff in the arraylist; 
+    % will calculate all these max,min,avg & etc. 
+
+*/
+
+void calculate(ArrayList *arrary_list);
+void calculate_clean(ArrayList *arrary_list);
+
+/* DISPLAY THE ARRAY */
 
 void arr_print(ArrayList arrary_list); 
-void arr_print_revece(ArrayList arrary_list);
+void arr_print_reverce(ArrayList arrary_list);
 void arr_print_extra(ArrayList arrary_list); // will display except int* vals
 
 
